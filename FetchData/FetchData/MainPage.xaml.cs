4a6ac83fcb5599f48ec49c9f645cb752a13e0780
Fernando Avelino <fernandoavelino94@gmail.com>
2021-11-30 19:11:14 -0300

    initial commit

---
+++ FetchData/FetchData/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FetchData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Login();
+        }
+        public async void Login()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://localhost:44365/api/Logins1");
+            var login = JsonConvert.DeserializeObject<List<Login>>(response);
+
+            ListLogin.ItemsSource = login;
+        }
+    }
+}
